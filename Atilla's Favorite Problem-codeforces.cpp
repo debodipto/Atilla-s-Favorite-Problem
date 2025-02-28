2b1b27116b3930f1cd018f5e17dc5fe15f1161ed
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-02-28 13:10:03 +0600

    Update Atilla's Favorite Problem-codeforces.cpp

--- Atilla's Favorite Problem-codeforces.cpp
+++ Atilla's Favorite Problem-codeforces.cpp
@@ -1,1 +1,24 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n;
+       cin>>n;
+       string s;
+       cin>>s;
+       char c='a';
+       for(int i=0;i<n;i++)
+       {
+           if(s[i]>c)
+           c = s[i];
+       }
+       cout<<abs(97-c)+1<<endl;
+    }
+    return 0;
+}
